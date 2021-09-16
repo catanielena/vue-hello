d8b15838671d38903c22b74e9f8a366a6c570ad5
catanielena <catani.elena1@gmail.com>
2021-09-16 16:08:33 +0200

    Add: style

--- js/main.js
+++ js/main.js
@@ -7,3 +7,31 @@ const app = new Vue (
         }
     }
 )
+
+function setup() {
+    sketchWidth = document.getElementById("box").offsetWidth;
+    sketchHeight = document.getElementById("box").offsetHeight;
+    createCanvas(sketchWidth, sketchHeight);
+  }
+  
+  function draw() {
+    blendMode(EXCLUSION);
+    textSize(30);
+    fill(255, 255, 0);
+    text("Hello", mouseX, mouseY);
+}
+
+function mouseClicked() {
+    textSize(30);
+    fill(255, 0, 0);
+    text("Hello", mouseX, mouseY);
+    // prevent default
+    return false;
+  }
+  
+  
+  function windowResized() {
+    sketchWidth = document.getElementById("box").offsetWidth;
+    sketchHeight = document.getElementById("box").offsetHeight;
+    resizeCanvas(sketchWidth, sketchHeight);
+  }
