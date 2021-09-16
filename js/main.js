90c2ed4868aa2b10b4a6ff1141594a2cd72293f1
catanielena <catani.elena1@gmail.com>
2021-09-16 15:06:30 +0200

    Add: message, img

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const app = new Vue (
+    {
+        el: "#root",
+        data: {
+            message:"Hello World",
+            baby: "https://awkwardfamilyphotos.com/wp-content/uploads/2021/03/Screen-Shot-2021-03-11-at-4.53.15-PM.jpg"
+        }
+    }
+)
